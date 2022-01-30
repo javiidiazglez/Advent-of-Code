2f59fad9c35496299354762b18ee75c2126b41b4
Javi <javiidiazglez@gmail.com>
2022-01-30 00:53:51 +0000

    subida parte 1 day01

---
+++ Day 01/include/puzzle_1.h
@@ -0,0 +1,9 @@
+class Puzzle_1 {
+  
+  public:
+    static void SolucionA();
+    
+  private:
+    static int ContadorMeasurements();
+
+};
---
+++ Day 01/src/main.cc
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "puzzle_1.h"
+
+int main(){
+
+  Puzzle_1::SolucionA();
+
+}
